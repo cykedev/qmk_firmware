c7f823fc3e20704e45844f42f31ff74a971e8ebd
Christian Eiden <christian@eiden.ch>
2021-02-26 08:26:36 +0100

    adjustments on personal keymap
    Signed-off-by: Christian Eiden <christian@eiden.ch>

--- keyboards/redox/keymaps/cykedev/keymap.c
+++ keyboards/redox/keymaps/cykedev/keymap.c
@@ -15,11 +15,12 @@
 
 #include QMK_KEYBOARD_H
 
-#define SY_SPA  LT(_SYMBOL, KC_SPC)
+#define CM_SPA  MT(MOD_LGUI, KC_SPC)
+#define AL_SPA  MT(MOD_RALT, KC_SPC)
+#define SY_COM  LT(_SYMBOL, KC_COMM)
+#define SY_DOT  LT(_SYMBOL, KC_DOT)
 #define SH_ENL  MT(MOD_LSFT, KC_ENT)
 #define SH_ENR  MT(MOD_RSFT, KC_ENT)
-#define CM_COM  MT(MOD_LGUI, KC_COMM)
-#define CM_DOT  MT(MOD_RGUI, KC_DOT)
 #define ADJ     MO(_ADJUST)
 #define QWERT   DF(_QWERTY)
 #define COLEM   DF(_COLEMAK)
@@ -44,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|       |                    |       |------+------+------+------+------+------|
    * |Shift |   Z  |   X  |   C  |   V  |   B  '----------------,  ,----------------'   N  |   M  |   ,  |   .  |   /  | Shift|
    * |-----------------------------------------/   *   /   /   /    \   +   \   -   \-----------------------------------------|
-   * |  ADJ | Hypr |  Meh |  Alt |    /  Cmd  /-------/-------/      \-------\-------\  Cmd  \    | Alt  | PgUp | PgDn | ADJ  |
-   * |      |      |      |      |   /   ,   /  SYM  / Shift /        \ Shift \  SYM  \   .   \   |      |      |      |      |
+   * |  ADJ | Hypr |  Alt | Ctrl |    /  SYM  /-------/-------/      \-------\-------\  SYM  \    | Ctrl | Alt  | Meh  | ADJ  |
+   * |      |      |      |      |   /   ,   /  Cmd  / Shift /        \ Shift \  Alt  \   .   \   |      |      |      |      |
    * `---------------------------'  '-------/ Space / Enter /          \ Enter \ Space \-------'  '---------------------------'
    *                                        '--------------'            '--------------'
    */
@@ -53,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB  , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_HOME ,                         KC_END , KC_Y   , KC_U   , KC_I    , KC_O    , KC_P    , KC_BSLS ,
      KC_LCTL , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_DEL  ,                         KC_BSPC, KC_H   , KC_J   , KC_K    , KC_L    , KC_SCLN , KC_QUOT ,
      KC_LSFT , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_ASTR , KC_SLSH   , KC_PPLS   , KC_MINS, KC_N   , KC_M   , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT ,
-     ADJ     , KC_HYPR, KC_MEH ,KC_LALT ,          CM_COM , SY_SPA  , SH_ENL    , SH_ENR    , SY_SPA , CM_DOT ,          KC_RALT , KC_PGUP , KC_PGDN , ADJ
+     ADJ     , KC_HYPR, KC_LALT, KC_LCTL,          SY_COM , CM_SPA  , SH_ENL    , SH_ENR    , AL_SPA , SY_DOT ,          KC_RCTL , KC_RALT , KC_MEH  , ADJ
   ),
 
   [_COLEMAK] = LAYOUT(
@@ -67,8 +68,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|       |                    |       |------+------+------+------+------+------|
    * |Shift |   Z  |   X  |   C  |   V  |   B  '----------------,  ,----------------'   K  |   M  |   ,  |   .  |   /  | Shift|
    * |-----------------------------------------/   *   /   /   /    \   +   \   -   \-----------------------------------------|
-   * |  ADJ | Hypr |  Meh |  Alt |    /  Cmd  /-------/-------/      \-------\-------\  Cmd  \    | Alt  | PgUp | PgDn | ADJ  |
-   * |      |      |      |      |   /   ,   /  SYM  / Shift /        \ Shift \  SYM  \   .   \   |      |      |      |      |
+   * |  ADJ | Hypr |  Alt | Ctrl |    /  SYM  /-------/-------/      \-------\-------\  SYM  \    | Ctrl | Alt  | Meh  | ADJ  |
+   * |      |      |      |      |   /   ,   /  Cmd  / Shift /        \ Shift \  Alt  \   .   \   |      |      |      |      |
    * `---------------------------'  '-------/ Space / Enter /          \ Enter \ Space \-------'  '---------------------------'
    *                                        '--------------'            '--------------'
    */
@@ -76,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB  , KC_Q   , KC_W   , KC_F   , KC_P   , KC_G   , KC_HOME ,                         KC_END , KC_J   , KC_L   , KC_U    , KC_Y    , KC_SCLN , KC_BSLS ,
      KC_LCTL , KC_A   , KC_R   , KC_S   , KC_T   , KC_D   , KC_DEL  ,                         KC_BSPC, KC_H   , KC_N   , KC_E    , KC_I    , KC_O    , KC_QUOT ,
      KC_LSFT , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_ASTR , KC_SLSH   , KC_PPLS   , KC_MINS, KC_K   , KC_M   , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT ,
-     ADJ     , KC_HYPR, KC_MEH ,KC_LALT ,          CM_COM , SY_SPA  , SH_ENL    , SH_ENR    , SY_SPA , CM_DOT ,          KC_RALT , KC_PGUP , KC_PGDN , ADJ
+     ADJ     , KC_HYPR, KC_LALT, KC_LCTL,          SY_COM , CM_SPA  , SH_ENL    , SH_ENR    , AL_SPA , SY_DOT ,          KC_RCTL , KC_RALT , KC_MEH  , ADJ
   ),
 
   [_SYMBOL] = LAYOUT(
@@ -99,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______ , KC_EXLM , KC_AT   , KC_LCBR , KC_RCBR , KC_PIPE , KC_PGUP ,                     KC_PGDN , KC_AMPR , KC_PPLS , KC_UP   , XXXXXXX , XXXXXXX , KC_F12  ,
      _______ , KC_HASH , KC_DLR  , KC_LPRN , KC_RPRN , KC_GRV  , KC_INS  ,                     _______ , KC_PEQL , KC_LEFT , KC_DOWN , KC_RGHT , KC_QUES , XXXXXXX ,
      XXXXXXX , KC_PERC , KC_CIRC , KC_LBRC , KC_RBRC , KC_TILD , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_ASTR , KC_MINS , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
-     XXXXXXX , _______ , _______ , _______ ,           _______ , _______ , _______ , _______ , _______ , _______ ,           _______ , XXXXXXX , XXXXXXX , XXXXXXX
+     XXXXXXX , _______ , _______ , _______ ,           _______ , _______ , _______ , _______ , _______ , _______ ,           _______ , _______ , _______ , XXXXXXX
   ),
 
   [_ADJUST] = LAYOUT(
@@ -128,9 +129,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case CM_COM:
+        case SY_COM:
             return TAPPING_TERM - 50;
-        case CM_DOT:
+        case SY_DOT:
             return TAPPING_TERM - 50;
         default:
             return TAPPING_TERM;
