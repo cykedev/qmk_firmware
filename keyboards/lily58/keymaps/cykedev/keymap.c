e5c2278e4d12de5833dc884b02b81a659fffaaae
Christian Eiden <christian@eiden.ch>
2021-03-08 16:45:31 +0100

    adjustments on personal keymap
    Signed-off-by: Christian Eiden <christian@eiden.ch>

--- keyboards/lily58/keymaps/cykedev/keymap.c
+++ keyboards/lily58/keymaps/cykedev/keymap.c
@@ -15,10 +15,10 @@
 
 #include QMK_KEYBOARD_H
 
-#define AL_SPA  MT(MOD_RALT, KC_SPC)
+#define SY_SPA  LT(_SYMBOL,  KC_SPC)
 #define SH_ENT  MT(MOD_LSFT, KC_ENT)
 #define CM_COM  MT(MOD_LGUI, KC_COMM)
-#define SY_DOT  LT(_SYMBOL, KC_DOT)
+#define AL_DOT  MT(MOD_RALT, KC_DOT)
 #define ADJ     MO(_ADJUST)
 #define QWERT   DF(_QWERTY)
 #define COLEM   DF(_COLEMAK)
@@ -41,8 +41,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Ctrl |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|  Del  |    | Back  |------+------+------+------+------+------|
  * |Shift |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  | Shift|
- * `-----------------------------------------/ Shift /     \  Alt \-----------------------------------------'
- *                   |  Alt | Ctrl |  Cmd | / Enter /       \ Space\  |  SYM | Ctrl | ADJ  |
+ * `-----------------------------------------/ Shift /     \  SYM \-----------------------------------------'
+ *                   |  Alt | Ctrl |  Cmd | / Enter /       \ Space\  |  Alt | Ctrl | ADJ  |
  *                   |      |      |   ,  |/       /         \      \ |   .  |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,   KC_DEL,    KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                             KC_LALT, KC_LCTL, CM_COM, SH_ENT,    AL_SPA,  SY_DOT,  KC_RCTL, ADJ
+                             KC_LALT, KC_LCTL, CM_COM, SH_ENT,    SY_SPA,  AL_DOT,  KC_RCTL, ADJ
 ),
  /* COLEMAK
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -63,8 +63,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Ctrl |   A  |   R  |   S  |   T  |   D  |-------.    ,-------|   H  |   N  |   E  |   I  |   O  |  '   |
  * |------+------+------+------+------+------|  Del  |    | Back  |------+------+------+------+------+------|
  * |Shift |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   K  |   M  |   ,  |   .  |   /  | Shift|
- * `-----------------------------------------/ Shift /     \  Alt \-----------------------------------------'
- *                   |  Alt | Ctrl |  Cmd | / Enter /       \ Space\  |  SYM | Ctrl | ADJ  |
+ * `-----------------------------------------/ Shift /     \  SYM \-----------------------------------------'
+ *                   |  Alt | Ctrl |  Cmd | / Enter /       \ Space\  |  Alt | Ctrl | ADJ  |
  *                   |      |      |   ,  |/       /         \      \ |   .  |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -74,17 +74,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                       KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
   KC_LCTL,  KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                       KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,   KC_DEL,    KC_BSPC, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                             KC_LALT, KC_LCTL, CM_COM, SH_ENT,    AL_SPA,  SY_DOT,  KC_RCTL, ADJ
+                             KC_LALT, KC_LCTL, CM_COM, SH_ENT,    SY_SPA,  AL_DOT,  KC_RCTL, ADJ
 ),
 /* SYMBOL
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  `~  |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   !  |   @  |   {  |   }  |   |  |                    |  &   |   +  |  up  |      |      | F12  |
+ * |      |   !  |   @  |   {  |   }  |   +  |                    |      |      |  up  |      |      | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   #  |   $  |   (  |   )  |   `  |-------.    ,-------|  =   | left | down |right |  ?   |      |
+ * |      |   &  |   |  |   (  |   )  |   -  |-------.    ,-------|      | left | down |right |  ?   |      |
  * |------+------+------+------+------+------|  Ins  |    |       |------+------+------+------+------+------|
- * |      |   %  |   ^  |   [  |   ]  |   ~  |-------|    |-------|  *   |   -  |      |      |      |      |
+ * |      |   *  |   /  |   [  |   ]  |   =  |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |      |      |      | /       /       \      \  |      |      |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -92,9 +92,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_SYMBOL] = LAYOUT(
   KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,                   KC_AMPR, KC_PPLS, KC_UP,   XXXXXXX, XXXXXXX, KC_F12,
-  _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,                    KC_PEQL, KC_LEFT, KC_DOWN, KC_RGHT, KC_QUES, XXXXXXX,
-  XXXXXXX, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_INS,  _______, KC_ASTR, KC_MINS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PPLS,                   XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, KC_F12,
+  _______, KC_AMPR, KC_PIPE, KC_LPRN, KC_RPRN, KC_MINS,                   XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_QUES, XXXXXXX,
+  XXXXXXX, KC_ASTR, KC_SLSH, KC_LBRC, KC_RBRC, KC_PEQL, KC_INS,  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                              _______, _______, _______, _______, _______,  _______, _______, _______
 ),
 /* ADJUST
@@ -124,7 +124,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case CM_COM:
             return TAPPING_TERM - 50;
-        case SY_DOT:
+        case AL_DOT:
             return TAPPING_TERM - 50;
         default:
             return TAPPING_TERM;
